6dfab71ad1fc29af7a21a048f790a446c0d1db89
Austin Gray <agray@csumb.edu>
2017-10-03 05:56:26 +0000

    final changes

--- index.php
+++ index.php
@@ -1,59 +1,40 @@
-<?php
-    $backgroundImage = "./img/sea.jpg"; 
-    
-    
-    function getTenRandomImages($imgURLs) {
-        // for now, return the first 10 images 
-        $imagesToDisplay = array_slice($imgURLs, 0, 10); 
-        return $imagesToDisplay; 
-    }
-    
-    
-    if (isset($_GET['keyword'])) {
-        // show carousel
+ <?php
+            $backgroundImage = "img/sea.jpg";
+            
+            if(isset($_GET['keyword'])){
+                include './api/pixabayAPI.php';
+                $keyword = $_GET['keyword'];
+                $imgUrls = getImageURLs($keyword);
+                $backgroundImage = $imgUrls[array_rand($imgUrls)];
+            }
+        ?>
+      
         
-        // make request to pixabay
-        include "./api/pixabayAPI.php"; 
-        
-        $imgURLs = getImageURLs($_GET['keyword']); 
-        $imgsToDisplay = getTenRandomImages($imgURLs); 
-        
-        
-        
-        // set random background image 
-        $backgroundImage = $imgsToDisplay[array_rand($imgsToDisplay)]; 
-    } 
-    
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
+        <title> Image Carousel! </title>
+        <meta charset"utf=8">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        
         <style>
-            @import url("./css/styles.css"); 
-        </style>
-        
-        <style>
-            body {
-                background-image: url("<?=$backgroundImage?>");
+            @import url("css/styles.css");
+            body{
+                background-image: url('<?=$backgroundImage?>');
             }
         </style>
     </head>
     <body>
-        
+        <br/> <br/>
         <?php 
-            if (!isset($imgsToDisplay)) {
+            if (!isset($imgUrls)) {
                 // show prompt to user to enter query
-                echo "<h2> Enter query to see imagees from Pixabay</h2>"; 
+                echo "<h2> Search images from Pixabay!</h2>"; 
                     
             } else {
                 // show carousel
                     echo '<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">'; 
                     echo '<ol class="carousel-indicators"> '; 
-                    for ($i = 0; $i < count($imgsToDisplay); $i++) {
+                    for ($i = 0; $i < 7; $i++) {
                         echo '<li data-target="#carousel-example-generic" data-slide-to="'.$i.'"'; 
                         echo $i == 0 ? 'class="active"' : ''; 
                         echo '></li>'; 
@@ -62,35 +43,30 @@
                     
                     echo '<div class="carousel-inner" role="listbox">'; 
                     
-                    for ($i = 0; $i < count($imgsToDisplay); $i++) {
+                    for ($i = 0; $i < 7; $i++) {
                         echo '<div class="item '; 
                         echo $i == 0 ? 'active' : ''; 
                         echo '">'; 
-                        echo '<img src="'.$imgsToDisplay[$i].'" alt="...">'; 
+                        echo '<img src="'.$imgUrls[$i].'" alt="...">'; 
                         echo '</div>';     
                     } 
                 
                     echo '</div>'; 
                 ?>
-                
-                <!-- Controls -->
-              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-              
-        </div>    
-        <?php
-                
-            } // finishes the else 
-            
+                </div>
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        <?php  
+                }   
         ?>
-        
-        
+        <br>
         <form>
             <input type= "text" name="keyword" placeholder="Keyword" value="<?=$_GET['keyword']?>" />
             <input type= "radio" id = "lhorizontal" name = "layout" value = "horizontal" >
@@ -107,7 +83,7 @@
             <input type= "submit" value="Search" />
         </form>
         
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
